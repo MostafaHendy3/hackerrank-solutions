26af84954128a6eb9a45c88d9779497c40878158
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2026-07-08 01:18:39 +0300

    ✅ Japanese Cities' Attributes [sql] — approach1

---
+++ solutions/sql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,20 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-07-08, 01:18 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT *
+from CITY
+where CountryCode = 'JPN' ;
+

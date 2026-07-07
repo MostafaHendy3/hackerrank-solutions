34d37b6cb47ed49a3b1296aa8cc009536e14eb2a
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2026-07-08 01:33:52 +0300

    ✅ Weather Observation Station 1 [sql] — approach1

---
+++ solutions/sql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,19 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-07-08, 01:33 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT CITY  , STATE 
+from station
+

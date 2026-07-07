010fe1552057216eb3c51bd46fb8dfa6863d8ac7
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2026-07-08 01:16:13 +0300

    ✅ Select By ID [sql] — approach1

---
+++ solutions/sql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,20 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-07-08, 01:16 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT *
+from CITY
+where ID = '1661';
+
